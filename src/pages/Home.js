8902348c63b570bfbe512a87e0bc65d939ba642b
Aamir Bhat <aamirbhat382@gmail.com>
2022-07-22 05:02:24 +0000

    final commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect,useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from "react-router-dom"
 import ListComponent from '../componentss/ListComponent'
 
@@ -12,18 +12,19 @@ function Home() {
       setLoginUser(true)
     }
 
-  },[])
+  }, [])
+
+const logOut = ()=>{
+  localStorage.removeItem('user')
+  window.location.reload();
+}
+
   return (
     <>
       <header>
         <div className='login_wrapper'>
-
-    <Link className='login_button' to="/logout">Logout</Link>
-    
-           
-
-            {/* <Link className='login_button' to="/login">Login</Link> */}
-         
+          <button className='login_button' onClick={logOut} style={{ display: loginUser ? "" : "none" }}>Logout</button>
+          <Link className='login_button' to="/login" style={{ display: loginUser ? "none" : "" }}>Login</Link>
         </div>
 
       </header>
